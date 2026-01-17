5a72a60d325ee641581d750169afa3f5bc61193c
Artie Kurtenoks <artema2@gmail.com>
2026-01-17 17:13:03 +0700

    created gitignore and parquet file

--- Module 1 - Docker and Terraform/pipelines/pipeline.py
+++ Module 1 - Docker and Terraform/pipelines/pipeline.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import os
 
 print('System arguments', sys.argv)
 
@@ -11,4 +12,7 @@ print(f'hello pipeline, month= {month}')
 df = pd.DataFrame({"day" : [1,2], "num_passengers":[3,4]})
 df["month"] = month
 
+df.to_parquet(f"output_{month}.parquet")
+
 print(df.head())
+print(os.getcwd())
